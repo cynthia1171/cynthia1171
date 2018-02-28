160acb45ec6057745675af14e4338a379bdaf9a1
Cynthia Isla <c.isla1171@gmail.com>
2018-02-28 20:55:46 -0300

    skill y contacto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
-    $('.parallax').parallax();
-  });
+  $('.button-collapse').sideNav();
+  $('.parallax').parallax();
+  $('.scrollspy').scrollSpy();
 
-$(".button-collapse").sideNav();
+  $('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 500);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(500);
+		} else {
+			$('.ir-arriba').slideUp(500);
+		}
+  });
+  
+  $(function(){
+    $('a[href*="#"]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+          var targetOffset = $target.offset().top;
+          $('html,body').animate({scrollTop: targetOffset}, 1000);
+          return false;
+        }
+      }
+    });
+  });
+});
