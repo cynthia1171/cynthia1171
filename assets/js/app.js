27272e943d78054a3f6fae5e40637051f582d6e4
cynthia1171 <c.isla1171@gmail.com>
2018-03-17 14:49:26 -0300

    portafolio nuevo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+  $(".button-collapse").sideNav();
+  $(".scrollspy").scrollSpy();
+
+  $(".ir-arriba").click(function() {
+    $("body, html").animate({ scrollTop: "0px" }, 500);
+  });
+  $(".tooltipped").tooltip({ delay: 50 });
   $(".p2").typed({
     strings: ["CYNTHIA", "FRONTEND", "DEVELOPER", "GAMER"],
     typeSpeed: 50,
@@ -7,237 +14,140 @@ $(document).ready(function() {
     showCursor: false,
     loop: true
   });
-  $(".rotator").smoove({
-    offset: "40%"
-  });
 
   /* funcion particles de slider */
 
-  var particleNumber = 75;
-  // number of particles (change it!)
+  $(function() {
+    var canvas = document.querySelector("canvas"),
+      ctx = canvas.getContext("2d"),
+      color = "rgba(255, 255, 255, .5)";
 
-  window.requestAnimFrame = (function() {
-    return (
-      window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      function(callback) {
-        window.setTimeout(callback, 1000 / 60);
-      }
-    );
-  })();
-  // requesting the keyframes
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    canvas.width = w;
+    canvas.height = h;
+    ctx.fillStyle = color;
+    ctx.lineWidth = 0.1;
+    ctx.strokeStyle = color;
 
-  var c = document.getElementById("c");
-  var ctx = c.getContext("2d");
-  //context and id of canvas
+    var mousePosition = { x: 10 * w / 100, y: 10 * h / 100 };
 
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  //width and height of canvas
+    var dots = {
+      num: 100,
+      distance: 200,
+      d_radius: 200,
+      velocity: -0.9,
+      array: []
+    };
 
-  c.width = w;
-  c.height = h;
-  //setting the width and height for canvas
+    function Dot() {
+      this.x = Math.random() * w;
+      this.y = Math.random() * h;
 
-  var mouse = { x: w / 2, y: h / 2 };
-  //mouse position
+      this.vx = dots.velocity + Math.random();
+      this.vy = dots.velocity + Math.random();
 
-  document.addEventListener(
-    "mousemove",
-    function(e) {
-      mouse.x = e.clientX || e.pageX;
-      mouse.y = e.clientY || e.pageY;
-    },
-    false
-  );
-  //finding the mouse position
+      this.radius = Math.random() * 2;
+    }
 
-  //when clicked, the set-up function runs
-  document.addEventListener(
-    "mouseover",
-    function() {
-      var particleNumber = 75;
+    Dot.prototype = {
+      create: function() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fill();
+      },
+      animate: function() {
+        for (i = 0; i < dots.num; i++) {
+          var dot = dots.array[i];
 
-      var particles = [];
-      // the particles storage
-
-      for (i = 0; i < particleNumber; i++) {
-        setTimeout(function() {
-          particles.push(new createParticle());
-        }, i * 10);
-        // add a particle (not all at once - setTimeout(); )
-      }
-      // adding 55 particles
-
-      function createParticle() {
-        this.x = mouse.x;
-        this.y = mouse.y;
-        // the x and y
-
-        this.vx = Math.random() * 8 - 4;
-        this.vy = Math.random() * 8 - 4;
-        // the velocities
-
-        this.size = 20;
-        // the size
-
-        this.life = Math.random() * 100;
-        // the life
-
-        var g = "rgba(46, 204, 113,1.0)";
-        var gg = "rgba(46, 204, 113,0.45)";
-        var ggg = "rgba(46, 204, 113,0.2)";
-        var array = [g, gg, ggg];
-        this.color = array[Math.floor(Math.random() * 3)];
-        // making 3 mandatory colors (change it and fork - i might use that!)
-
-        this.reset = function() {
-          // the reset
-
-          this.x = mouse.x;
-          this.y = mouse.y;
-          this.vx = Math.random() * 8 - 4;
-          this.vy = Math.random() * 8 - 4;
-          this.size = 20;
-          this.life = Math.random() * 33;
-        };
-      }
-
-      function draw() {
-        requestAnimFrame(draw);
-        // requesting the keyframes
-
-        ctx.fillStyle = "#262626";
-        ctx.fillRect(0, 0, c.width, c.height);
-        // the canvas
-
-        for (t = 0; t < particles.length; t++) {
-          var p = particles[t];
-          // using the particle we want to use
-
-          ctx.beginPath();
-          ctx.fillStyle = p.color;
-          ctx.fillRect(p.x, p.y, p.size, p.size);
-          // making the particle
-
-          p.x += p.vx;
-          p.y += p.vy;
-          // velocities
-
-          p.life *= 0.9;
-          p.size *= 0.89999;
-          // making the life and size decrease
-
-          if (p.life < 1) {
-            p.reset();
+          if (dot.y < 0 || dot.y > h) {
+            dot.vx = dot.vx;
+            dot.vy = -dot.vy;
+          } else if (dot.x < 0 || dot.x > w) {
+            dot.vx = -dot.vx;
+            dot.vy = dot.vy;
           }
-          // reseting the particles when dead
+          dot.x += dot.vx;
+          dot.y += dot.vy;
+        }
+      },
+      line: function() {
+        for (i = 0; i < dots.num; i++) {
+          for (j = 0; j < dots.num; j++) {
+            i_dot = dots.array[i];
+            j_dot = dots.array[j];
+
+            if (
+              i_dot.x - j_dot.x < dots.distance &&
+              i_dot.y - j_dot.y < dots.distance &&
+              i_dot.x - j_dot.x > -dots.distance &&
+              i_dot.y - j_dot.y > -dots.distance
+            ) {
+              if (
+                i_dot.x - mousePosition.x < dots.d_radius &&
+                i_dot.y - mousePosition.y < dots.d_radius &&
+                i_dot.x - mousePosition.x > -dots.d_radius &&
+                i_dot.y - mousePosition.y > -dots.d_radius
+              ) {
+                ctx.beginPath();
+                ctx.moveTo(i_dot.x, i_dot.y);
+                ctx.bezierCurveTo(
+                  i_dot.x,
+                  h / 2,
+                  w / 2,
+                  i_dot.y,
+                  j_dot.x,
+                  j_dot.y
+                );
+                ctx.stroke();
+                ctx.closePath();
+              }
+            }
+          }
         }
       }
-
-      draw();
-      // calling draw
-    },
-    false
-  );
-
-  //                 //
-  // background code //
-  //                 //
-
-  var particles = [];
-  // the particles storage
-
-  for (i = 0; i < particleNumber; i++) {
-    setTimeout(function() {
-      particles.push(new createParticle());
-    }, i * 15);
-    // add a particle (not all at once - setTimeout(); )
-  }
-  // adding 55 particles
-
-  function createParticle() {
-    this.x = Math.random() * c.width;
-    this.y = Math.random() * c.height;
-    // the x and y
-
-    this.vx = Math.random() * 8 - 4;
-    this.vy = Math.random() * 8 - 4;
-    // the velocities
-
-    this.size = 20;
-    // the size
-
-    this.life = Math.random() * 100;
-    // the life
-
-    var g = "rgba(46, 204, 113,1.0)";
-    var gg = "rgba(46, 204, 113,0.45)";
-    var ggg = "rgba(46, 204, 113,0.2)";
-    var array = [g, gg, ggg];
-    this.color = array[Math.floor(Math.random() * 3)];
-    // making 3 mandatory colors (change it and fork - i might use that!)
-
-    this.reset = function() {
-      // the reset
-
-      this.x = Math.random() * c.width;
-      this.y = Math.random() * c.height;
-      this.vx = Math.random() * 8 - 4;
-      this.vy = Math.random() * 8 - 4;
-      this.size = 20;
-      this.life = Math.random() * 33;
     };
-  }
 
-  function draw() {
-    requestAnimFrame(draw);
-    // requesting the keyframes
+    function createDots() {
+      ctx.clearRect(0, 0, w, h);
+      for (i = 0; i < dots.num; i++) {
+        dots.array.push(new Dot());
+        dot = dots.array[i];
 
-    ctx.fillStyle = "#262626";
-    ctx.fillRect(0, 0, c.width, c.height);
-    // the canvas
-
-    for (t = 0; t < particles.length; t++) {
-      var p = particles[t];
-      // using the particle we want to use
-
-      ctx.beginPath();
-      ctx.fillStyle = p.color;
-      ctx.fillRect(p.x, p.y, p.size, p.size);
-      // making the particle
-
-      p.x += p.vx;
-      p.y += p.vy;
-      // velocities
-
-      p.life *= 0.9;
-      p.size *= 0.89999;
-      // making the life and size decrease
-
-      if (p.life < 1) {
-        p.reset();
+        dot.create();
       }
-      // reseting the particles when dead
-    }
-  }
 
-  draw();
+      dot.line();
+      dot.animate();
+    }
+
+    $("canvas").on("mousemove mouseleave", function(e) {
+      if (e.type == "mousemove") {
+        mousePosition.x = e.pageX;
+        mousePosition.y = e.pageY;
+      }
+      if (e.type == "mouseleave") {
+        mousePosition.x = w / 2;
+        mousePosition.y = h / 2;
+      }
+    });
+
+    $("canvas").on("click", function() {
+      dots.num += 10;
+    });
+
+    setInterval(createDots, 1000 / 30);
+
+    $(window).on("resize", function() {
+      canvas.width = w;
+      canvas.height = h;
+      ctx.fillStyle = color;
+      ctx.lineWidth = 0.1;
+      ctx.strokeStyle = color;
+    });
+  });
 
   /* fin slider */
-
-  $(".button-collapse").sideNav();
-  $(".scrollspy").scrollSpy();
-
-  $(".ir-arriba").click(function() {
-    $("body, html").animate(
-      {
-        scrollTop: "0px"
-      },
-      500
-    );
-  });
 
   $(window).scroll(function() {
     if ($(this).scrollTop() > 0) {
